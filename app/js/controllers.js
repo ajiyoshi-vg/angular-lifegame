5bd25c61b86bc416815487857e13ce13174f11f1
ajiyoshi-vg <Yoichi_Sudo@voyagegroup.com>
2013-11-27 21:34:00 +0900

    一旦動いた

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,49 @@
 'use strict';
 
+
+function comparePoint(lhs, rhs) {
+    if( !lhs && !rhs){
+        return 0;
+    }else if( !lhs ){
+        return -1;
+    }else if( !rhs ){
+        return 1;
+    }else if( lhs.x === rhs.x && lhs.y === rhs.y ){
+        return 0;
+    }else if( lhs.x < rhs.x ) {
+        return -1;
+    }else{
+        return (lhs.y < rhs.y) ? -1 : 1;
+    }
+}
+
+function uniq(list, comparator) {
+    var last = null;
+    return list.sort(comparator).reduce(function(acc, item) {
+        console.log(acc);
+        if( comparator(last, item) === 0 ){
+            return acc;
+        } else {
+            last = item;
+            return acc.concat(item);
+        }
+    }, []);
+}
+function point(X, Y) {
+    return { x:X, y:Y };
+}
 function integers(from, to) {
     var ret = [];
     for( var i=0, len=to-from; i<len; i++ ){
         ret[i] = from + i;
     }
     return ret;
+}
+
+function flatten(listOfList) {
+    return listOfList.reduce(function(acc, ls) {
+        return acc.concat(ls);
+    }, [] );
 }
 
 function surround( pt ) {
@@ -18,26 +56,44 @@ function surround( pt ) {
 
 function World(alives) {
     this.alives = alives;
-    this.isAlive = function(x, y) {
+    this.isAliveAt = function(there) {
         var n = this.alives.filter(function(pt){
-            return pt.x == x && pt.y == y;
+            return pt.x == there.x && pt.y == there.y;
         }).length;
        return n > 0;
     }
 
-    this.next = function() {
-        /*
-        var target = this.alives.map(function(cell){
-            return surround(cell);
-        });
+    this.countAliveCell = function(cells) {
         var me = this;
-        target.filter(function(cell){
-            if( me.isAlive(cell) ){
-                var n = me.countLiveCell(surround(cell)) ;
-                return n == 2 || n == 3;
+        var n = cells.reduce( function(acc, pt) {
+            return ( me.isAliveAt(pt) ) ? acc + 1 : acc;
+        }, 0 );
+        return n;
+    }
+
+    this.willAlive = function(cell) {
+        var n = this.countAliveCell( surround(cell) );
+        return n === 2 || n === 3;
+    }
+    this.willSpawn = function(cell) {
+        var n = this.countAliveCell( surround(cell) );
+        return n === 3;
+    }
+    this.next = function() {
+        var cmp = comparePoint;
+        var temp = flatten( this.alives.map(function(cell){
+            return surround(cell);
+        }));
+        var target = uniq(temp, comparePoint);
+        var me = this;
+        var nextAges = target.filter(function(cell){
+            if( me.isAliveAt(cell) ){
+                return me.willAlive(cell);
+            } else {
+                return me.willSpawn(cell);
             }
         });
-        */
+        this.alives = nextAges;
     }
 }
 
@@ -57,13 +113,17 @@ angular.module('myApp.controllers', []).
       $scope.integers = integers;
 
       var world = new World( [
-          { x:1, y:2 }, { x:3, y:4 }
+          point(1, 2), point(1, 3), point(1, 4)
           ] );
       $scope.isAlive = function(x, y) {
-          return world.isAlive(x, y);
+          return world.isAliveAt(point(x, y));
       };
 
       $scope.next = function() {
-          console.log('hoge');
+          world.next();
       };
+
+      $scope.num = function() {
+          return world.alives.length;
+      }
   });
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -19,8 +19,8 @@ describe('controllers', function(){
     var world = new World(
         [ {x:1, y:2} ]
     );
-    expect( world.isAlive(1, 2) ).toBe( true );
-    expect( world.isAlive(1, 3) ).toBe( false );
+    expect( world.isAliveAt(point(1, 2)) ).toBe( true );
+    expect( world.isAliveAt(point(1, 3)) ).toBe( false );
   }));
 
   it('should あるセルの周りにあるセルのリスト', inject(function() {
@@ -31,5 +31,45 @@ describe('controllers', function(){
       ]);
   }));
 
+  it('should セルのリストのうち生きているものを数える', inject(function() {
+      var cells =  surround( {x:1, y:2} ) ;
+      var world = new World( [
+          {x:0, y:1}, {x:1, y:1}
+      ] );
+      expect( world.countAliveCell(cells) ).toBe( 2 );
+      expect( world.countAliveCell([ point(0, 1) ]) ).toBe( 1 );
+  }));
+
+  it('should 配列の配列をフラットにする', inject(function() {
+      expect( flatten( [ [1, 2], [3] ] ) ).toEqual(
+          [ 1, 2, 3] );
+  }));
+
+  it('should 重複を排除した配列を得る', inject(function() {
+      var cmp = function(lhs, rhs) {
+          if( lhs === rhs ) {
+              return 0;
+          } else if( lhs < rhs ) {
+              return -1;
+          } else {
+              return 1;
+          }
+      };
+      expect( uniq([ 1, 1, 2 ], cmp) ).toEqual(
+          [ 1, 2 ]);
+      }));
+
+      it('should ポイント比較', inject(function() {
+          expect( comparePoint(point(1, 1), point(1, 1)) ).toBe(0);
+          expect( comparePoint(point(1, 1), point(2, 1)) ).toBeLessThan(0);
+          expect( comparePoint(point(1, 1), point(2, 0)) ).toBeLessThan(0);
+          expect( comparePoint(point(1, 1), point(1, 0)) ).toBeGreaterThan(0);
+          expect( comparePoint(point(1, 1), point(1, 2)) ).toBeLessThan(0);
+      }));
+      it('should ポイント比較nullが一番小さい', inject(function() {
+          expect( comparePoint(null, point(1, 1)) ).toBeLessThan(0);
+          expect( comparePoint(null, null) ).toBe(0);
+          expect( comparePoint(point(1, 1), null) ).toBeGreaterThan(0);
+      }));
 
 });
