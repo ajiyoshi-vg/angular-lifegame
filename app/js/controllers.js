cfea2fdb7966202fa30a33243fd77dda4ea36526
ajiyoshi-vg <Yoichi_Sudo@voyagegroup.com>
2013-11-27 19:43:39 +0900

    幅、高さをしていたテーブルを作りかけ

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,13 @@
 'use strict';
 
+function integers(from, to) {
+    var ret = [];
+    for( var i=0, len=to-from; i<len; i++ ){
+        ret[i] = from + i;
+    }
+    return ret;
+}
+
 /* Controllers */
 
 angular.module('myApp.controllers', []).
@@ -8,4 +16,10 @@ angular.module('myApp.controllers', []).
   }])
   .controller('MyCtrl2', [function() {
 
-  }]);
+  }])
+  .controller('Lifegame', function($scope){
+      $scope.width = 40;
+      $scope.height = 20;
+
+      $scope.integers = integers;
+  });
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -6,8 +6,12 @@ describe('controllers', function(){
   beforeEach(module('myApp.controllers'));
 
 
-  it('should ....', inject(function() {
+  it('should [0, 1, 2, 3... x] みたいな配列を作る', inject(function() {
     //spec body
+    expect( integers(0, 3) ).toEqual( [ 0, 1, 2 ] );
+    expect( integers(0, 4) ).toEqual( [ 0, 1, 2, 3 ] );
+    expect( integers(4, 2) ).toEqual( [ ] );
+    expect( integers(4, 4) ).toEqual( [ ] );
   }));
 
   it('should ....', inject(function() {
