22e77e9c9c7006a445b69aafde37a8bf6686854b
ajiyoshi-vg <Yoichi_Sudo@voyagegroup.com>
2013-11-27 20:26:32 +0900

    休憩前

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,6 +8,39 @@ function integers(from, to) {
     return ret;
 }
 
+function surround( pt ) {
+    return [
+        {x:pt.x-1, y:pt.y-1}, {x:pt.x, y:pt.y-1}, {x:pt.x+1, y:pt.y-1},
+        {x:pt.x-1, y:pt.y},                        {x:pt.x+1, y:pt.y},
+        {x:pt.x-1, y:pt.y+1}, {x:pt.x, y:pt.y+1}, {x:pt.x+1, y:pt.y+1}
+    ];
+}
+
+function World(alives) {
+    this.alives = alives;
+    this.isAlive = function(x, y) {
+        var n = this.alives.filter(function(pt){
+            return pt.x == x && pt.y == y;
+        }).length;
+       return n > 0;
+    }
+
+    this.next = function() {
+        /*
+        var target = this.alives.map(function(cell){
+            return surround(cell);
+        });
+        var me = this;
+        target.filter(function(cell){
+            if( me.isAlive(cell) ){
+                var n = me.countLiveCell(surround(cell)) ;
+                return n == 2 || n == 3;
+            }
+        });
+        */
+    }
+}
+
 /* Controllers */
 
 angular.module('myApp.controllers', []).
@@ -18,8 +51,19 @@ angular.module('myApp.controllers', []).
 
   }])
   .controller('Lifegame', function($scope){
-      $scope.width = 40;
-      $scope.height = 20;
+      $scope.width = 10;
+      $scope.height = 8;
 
       $scope.integers = integers;
+
+      var world = new World( [
+          { x:1, y:2 }, { x:3, y:4 }
+          ] );
+      $scope.isAlive = function(x, y) {
+          return world.isAlive(x, y);
+      };
+
+      $scope.next = function() {
+          console.log('hoge');
+      };
   });
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -14,7 +14,22 @@ describe('controllers', function(){
     expect( integers(4, 4) ).toEqual( [ ] );
   }));
 
-  it('should ....', inject(function() {
+  it('should ライフゲームのある座標が生きてるか', inject(function() {
     //spec body
+    var world = new World(
+        [ {x:1, y:2} ]
+    );
+    expect( world.isAlive(1, 2) ).toBe( true );
+    expect( world.isAlive(1, 3) ).toBe( false );
   }));
+
+  it('should あるセルの周りにあるセルのリスト', inject(function() {
+      expect( surround( {x:1, y:2} ) ).toEqual( [
+          {x:0, y:1}, {x:1, y:1}, {x:2, y:1},
+          {x:0, y:2},             {x:2, y:2},
+          {x:0, y:3}, {x:1, y:3}, {x:2, y:3}
+      ]);
+  }));
+
+
 });
